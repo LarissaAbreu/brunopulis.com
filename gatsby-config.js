23fc4e6e5a8783437ca8e90afbf06bc7931e9c44
Bruno Pulis <bruno.pulis@gmail.com>
2019-03-10 23:50:24 -0300

    chore(netlify): Add plugins for netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,17 +9,26 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-styled-components",
     {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
       },
     },
+
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/img/favicon.png",
+      },
+    },
+    "gatsby-plugin-netlify",
   ],
 }
