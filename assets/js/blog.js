a072e09f17315bb9cae2af39d915f6b216257824
Bruno Pulis <bruno.pulis@monetizze.com.br>
2020-05-01 20:41:32 -0300

    feat: add new feature from darkmode and lazyload

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,3 +1,16 @@
+if('loading' in HTMLImageElement.prototype) {
+	const images = document.querySelectorAll('img.lazyload');
+
+	images.forEach(img => {
+		img.src = img.dataset.src;
+	})
+} else {
+	let script = document.createElement('script');
+	script.async = true;
+	script.src = "https://cdnjs.cloudflare.com/ajax/libs/lazysizes/4.1.8/lazysizes.min.js";
+
+	document.body.appendChild(script);
+}
 $(document).ready(function() {
 	
 	/* ======= Highlight.js Plugin ======= */ 
@@ -5,8 +18,8 @@ $(document).ready(function() {
     $('pre code').each(function(i, block) {
 	    hljs.highlightBlock(block);
 	 });
-
 });
+
 
 document.getElementById('toggle-box-checkbox').addEventListener('change', function(event) {
 	(event.target.checked) ? document.body.setAttribute('data-theme', 'dark') :
